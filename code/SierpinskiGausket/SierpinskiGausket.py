0d6c562d0f7d1fa58332df06c96d7d699b23010f
J Christopher Lentfer Kristiansen <202005794@uni.au.dk>
2025-02-04 17:00:21 +0100

    Finished commenting (ish) the code

--- code/SierpinskiGausket/SierpinskiGausket.py
+++ code/SierpinskiGausket/SierpinskiGausket.py
@@ -25,20 +25,58 @@ def timer(n: int) -> None:
 
 
 class sierpinski:
+    """
+    A class to contain all the functions to draw the sierpinski triangle. 
+    To actually draw the trinangle one only needs the function "makeTriangle".
+
+    Attributes:
+    -
+        n : int
+            How many recursion of the sierpinski triangle is needed.
+
+    """
+
     def __init__(self, n):
+       """ Initializes the Sierpinski class
+
+       Args:
+       -
+        n : int
+            Defines the number of default reccursions should be done
+       
+       """
        self.n = int(n) 
 
 
     def findMidTriangle(self, points: list, n: int) -> list:
+        """A recursive function that finds all the "inner" triangles of the sierpinski triangle.
+
+        Meaning it finds all the triangle consisting of the midpoints of the previous triangle
+
+        Args:
+        -
+            points : list 
+                A list of points (often a triangle) for which we find the "inner" triangle
+            
+            n : int
+                An integer to measure how far we are in the iteration of the function
+
+        Returns:
+        -   
+            list
+                A list of all the inner triangles until n = 0
+        """
         
+        # Find the midpoints
         midLeft = self.findMidpoint(points[0], points[2])
         midRight= self.findMidpoint(points[1], points[2])
         midBottom = self.findMidpoint(points[0], points[1])
 
-        # Stops at n-2, because we use interger division and midLeft and MidRight are at 1/4 * 2^n
+        # Stops at n-2, because we use interger division and midLeft and MidRight are at (1/4 * 2^n, 2^(n-1)) and (3/4 * 2^n, 2^(n-1))
         if n-2 == 0:
             return [[tuple(midLeft), tuple(midRight), tuple(midBottom), tuple(midLeft)]]
         else:
+            # Gives us the points we just found and run the function again. First on left triangle, then right triangle and lastly the top tirangle
             return [[tuple(midLeft), tuple(midRight), tuple(midBottom), tuple(midLeft)]] + self.findMidTriangle([points[0], midBottom, midLeft], n-1) + self.findMidTriangle([midBottom, points[1], midRight], n-1) + self.findMidTriangle([midLeft, midRight, points[2]], n-1) 
             
         
@@ -62,11 +100,13 @@ class sierpinski:
     def makeTriangle(self, n: int = None)-> None:
         """Given an amount of iterations gives a plot for the Sierpinski triangle after n iterations.
             The plot output will be in the scale of 2**n, to avoid float division and to take advantage of interger division
+
+            Futher the function "findMidTriangle" finds all the middle triangles, to draw as few lines as possible
         
         Args:
         -
-            n : int
-                Amount of iterations to run through 
+            n : int (default = None)
+                Amount of iterations to run through. If no input is given input from __init__ will be used.
         
         Returns:
         -
