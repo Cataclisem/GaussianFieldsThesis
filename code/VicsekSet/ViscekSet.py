449fbdc5fbb8fbebc40a22e2d8396119499fd2c2
J Christopher Lentfer Kristiansen <202005794@uni.au.dk>
2025-03-03 16:52:28 +0100

    Finally finished the proof of 2.2.1 (with execption of why |f_m(x)| =< C for some contant C

--- code/VicsekSet/ViscekSet.py
+++ code/VicsekSet/ViscekSet.py
@@ -1,6 +1,27 @@
 import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
 import numpy as np
+import timeit
+import math
+
+def timer(n: int) -> None:
+    """Prints the time for each iteration of the makeTriangle Function
+
+    Args:
+    -
+        n : int
+            To what iteration the loop should run
+
+    Returns:
+    -
+        None
+            Prints the time of each iteration
+    """
+    for i in range(n):
+        start = timeit.default_timer()
+        viscek(n=1).makeViscek(n = i)
+        stop = timeit.default_timer()
+        print(f"Iterations:  {i}  Time:  {stop - start}")
 
 
 class viscek:
@@ -27,8 +48,8 @@ class viscek:
         for x in endPoints:
              collection.extend([[y + x*2 for y in lists] for lists in points])
         
-        if n == 0:
-            return collection + points
+        if n-1 == 0:
+            return collection
 
         newEndPoints = [x + 2*x for x in endPoints]
         return collection + self.findPoints(newEndPoints, collection + points, n-1)
@@ -42,16 +63,26 @@ class viscek:
 
         init_length = 2
         init_val = [[np.array([-init_length, 0]), np.array([init_length, 0])], [np.array([0, init_length]), np.array([0,-init_length])]]
-        init_endPoints = [np.array([-init_length, 0]), np.array([init_length, 0]), np.array([0, init_length]), np.array([0,-init_length])]
 
-        test = self.findPoints(init_endPoints, init_val, n)
-
-        line_collection = [[tuple(x) for x in lists] for lists in test]
+        if n <= 0:
+            line_frac = 1
+            line_collection = init_val
+            midpoints = [(0,0)]
+        else:   
+            line_frac = n
+            init_endPoints = [np.array([-init_length, 0]), np.array([init_length, 0]), np.array([0, init_length]), np.array([0,-init_length])]
+            vicsekSet = self.findPoints(init_endPoints, init_val, n)
+            line_collection = [[tuple(x) for x in lists] for lists in vicsekSet] + [[tuple(x) for x in lists] for lists in init_val]
+            midpoints = set([tuple((x[0] + x[1]) // 2) for x in vicsekSet] + [(0,0)])
         #print(test)
+        
+        point_x = [x[0] for lists in line_collection for x in lists] + [x[0] for x in midpoints]
+        point_y = [y[1] for lists in line_collection for y in lists] + [y[1] for y in midpoints]
 
         # Create subplots
-        fig, ax = plt.subplots()
-        ax.add_collection(LineCollection(line_collection, colors = "black", linewidths=1/n))
+        fig, ax = plt.subplots(figsize=(10,10))
+        ax.add_collection(LineCollection(line_collection, colors = "black", linewidths=1/line_frac, capstyle="butt", joinstyle="round"))
+        ax.scatter(point_x, point_y, marker="o", s=20)
 
         # Computes limites of graph (how far x and y axis should stretch out) based on n
         buffer = (pow(3, n) * init_length)/10 # A buffer to make the final graph not look as cramped
@@ -64,8 +95,5 @@ class viscek:
         #plt.grid()
         plt.show()
 
-        
-        return None
-
-test = viscek(n=4)
-test.makeViscek()
+viscek(n=1).makeViscek(n=0)
+#timer(10)
